dca61ee1841afb7ca53d718d2511394d43964ed5
NKaelin <75902526+NKaelin@users.noreply.github.com>
2021-08-26 12:09:13 -0400

    built layout grid and header/navBar components

---
+++ src/components/headerNavBar/navBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return <div className="navBar"></div>;
+  }
+}
+
+export default NavBar;
---
+++ src/components/layout.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import Header from "./headerNavBar/header";
+import NavBar from "./headerNavBar/navBar";
+
+class Layout extends Component {
+  render() {
+    return (
+      <div className="layout">
+        {this.props.children}
+        <Header />
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default Layout;
