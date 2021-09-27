d7c35f1cae45f1c4dc526f5b9396eac6df7aec75
NKaelin <75902526+NKaelin@users.noreply.github.com>
2021-09-27 19:47:23 -0400

    built shop grids, finished back of shop product component

--- src/components/greenPriceTag.js
+++ src/components/greenPriceTag.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class GreenPriceTag extends Component {
   render() {
     const { className, title } = this.props;
-    return <div className={"${className} green-price-tag"}>${title}</div>;
+    return <div className={`${className} green-price-tag`}>${title}</div>;
   }
 }
 
